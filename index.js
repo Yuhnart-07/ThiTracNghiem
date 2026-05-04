f9b72d2b881eded5dd06f34472ee98317aee3158
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-04 21:22:42 +0700

    Kết nối CSDL

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express') // TƯƠNG TỰ LỆNH IMPORT
+require('dotenv').config();
 const path = require('path')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE); // KẾT NỐI CSDL
+
+const Tour = mongoose.model(
+  'Tour', 
+  { 
+    name: String,
+    time: String, 
+    vehicle: String
+  },
+  "tours" // TÊN COLLECTION
+);
+
 
 // THIẾT LẬP THƯ MỤC CHỨA FILE VIEW
 app.set('views', path.join(__dirname, 'views')); // PATH ĐỂ NỐI TÊN PROJECT VỚI /VIEW
@@ -18,11 +33,18 @@ app.get('/', (req, res) => {
   }) // RENDER DÙNG ĐỂ CHUYỂN TỪ CÚ PHÁP PUG SANG HTML
 });
 
-app.get('/tour', (req, res) => {
+app.get('/tour', async (req, res) => {
+  const tourList = await Tour.find({}); // AWAIT LÀ CHỜ LẤY DỮ LIỆU DÒNG NÀY XONG MỚI CHẠY XUỐNG DÒNG DƯỚI
+
+
   res.render('client/pages/tour-list', {
-    pageTitle: "Danh sách tour"
+    pageTitle: "Danh sách tour",
+    tourList: tourList
   })
 })
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
 })
+
+// phucnguyen1182005_db_user
+// NRPMxNzTlpJU4WaY
