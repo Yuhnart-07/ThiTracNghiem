50e1f34a03f201a639e9bee2be9efa64deef4315
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-04 16:06:07 +0700

    Tích hợp Pug

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express') // TƯƠNG TỰ LỆNH IMPORT
+const path = require('path')
 const app = express()
 const port = 3000
 
+// THIẾT LẬP THƯ MỤC CHỨA FILE VIEW
+app.set('views', path.join(__dirname, 'views')); // PATH ĐỂ NỐI TÊN PROJECT VỚI /VIEW
+
+// THIẾT LẬP PUB LÀM VIEW ENGINE
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render('client/pages/home') // RENDER DÙNG ĐỂ CHUYỂN TỪ CÚ PHÁP PUG SANG HTML
 })
 
 app.get('/tour', (req, res) => {
-  res.send('Danh sách tour')
+  res.render('client/pages/tour-list')
 })
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
