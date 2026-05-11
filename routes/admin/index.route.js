2c759be6e691ccf90c5a1905fd3b46c364daaa68
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-11 18:38:24 +0700

    Tích hợp user, contact

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,8 @@ const dashboardRoutes = require("./dashboard.route");
 const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 const orderRoutes = require("./order.route");
+const userRoutes = require("./user.route");
+const contactRoutes = require("./contact.route");
 
 
 router.use('/account', accountRoutes);
@@ -12,4 +14,12 @@ router.use('/dashboard', dashboardRoutes);
 router.use('/category', categoryRoutes);
 router.use('/tour',tourRoutes);
 router.use('/order',orderRoutes);
+router.use('/user',userRoutes);
+router.use('/contact',contactRoutes);
+
+
+
+
+
+
 module.exports = router;
