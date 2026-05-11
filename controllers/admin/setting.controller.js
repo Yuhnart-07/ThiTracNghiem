b0f463d0a1d64e840d3ca8313f830a10b094ff5b
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-11 19:00:07 +0700

    Tích hợp setting

---
+++ controllers/admin/setting.controller.js
@@ -0,0 +1,35 @@
+module.exports.list =  (req, res) => {
+  res.render('admin/pages/setting-list', {
+    pageTitle: "Cài đặt chung"
+  }) 
+};
+
+module.exports.websiteInfo =  (req, res) => {
+  res.render('admin/pages/setting-webiste-info', {
+    pageTitle: "Thông tin website"
+  }) 
+};
+
+module.exports.accountAdminList =  (req, res) => {
+  res.render('admin/pages/setting-account-admin-list', {
+    pageTitle: "Tài khoản quản trị"
+  }) 
+};
+
+module.exports.accountAdminCreate =  (req, res) => {
+  res.render('admin/pages/setting-account-admin-create', {
+    pageTitle: "Tạo tài khoản quản trị"
+  }) 
+};
+
+module.exports.roleList =  (req, res) => {
+  res.render('admin/pages/setting-role-list', {
+    pageTitle: "Nhóm quyền"
+  }) 
+};
+
+module.exports.roleCreate =  (req, res) => {
+  res.render('admin/pages/setting-role-create', {
+    pageTitle: "Tạo nhóm quyền"
+  }) 
+};
