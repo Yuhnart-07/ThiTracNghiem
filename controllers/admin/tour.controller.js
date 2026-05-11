8f61c9444fcbbae42bc2e14a7f42f72905c5541c
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-11 13:15:55 +0700

    Tích hợp trang category, oder, tour

---
+++ controllers/admin/tour.controller.js
@@ -0,0 +1,18 @@
+module.exports.list =  (req, res) => {
+  res.render('admin/pages/tour-list', {
+    pageTitle: "Quản lý tour"
+  }) 
+  
+};
+
+module.exports.create=  (req, res) => {
+  res.render('admin/pages/tour-create', {
+    pageTitle: "Tạo tour"
+  }) 
+};
+
+module.exports.trash =  (req, res) => {
+  res.render('admin/pages/tour-trash', {
+    pageTitle: "Thùng rác tour"
+  }) 
+};
