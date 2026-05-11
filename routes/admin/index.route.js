9cf58bde294d3a7e3d3ce88db20282a11588da4f
Phuc Nguyen <phucnguyen1182005@gmail.com>
2026-05-11 21:11:01 +0700

    Tích hợp 404 Not Found

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -20,7 +20,11 @@ router.use('/contact',contactRoutes);
 router.use('/setting',settingRoutes);
 router.use('/profile',profileRoutes);
 
-
+router.use((req,res) => {
+    res.render('admin/pages/error-404', {
+        pageTitle: "404 Not Found"
+    });
+});
 
 
 module.exports = router;
